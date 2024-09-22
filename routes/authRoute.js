0baab6b3376dcb4f41cf06a78ba834485ff11d84
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-22 15:24:19 +0530

    added express validation to the backend

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const { body,validationResult } = require('express-validator');
 
-router.post("/register", async(req,res)=>{
-    const { username,password, email, location } =  req.body;
+router.post("/register", [
+    body('username').isLength({ min:5 }),
+    body('email').isEmail(),
+    body('password', 'Password must be atleast of 5 characters').isLength({ min:5 })
+] ,async(req,res)=>{
+
+    const err = validationResult(req);
+    if(!err.isEmpty())
+        return res.status(400).json({err: err.array()})
+
     try{
+        const { username,password, email, location } =  req.body;
         await User.create({ 
             username, 
             email, 
