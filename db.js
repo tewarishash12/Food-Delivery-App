4dee94c9cba3de96f55f65aa3de45445fb04a0e1
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-27 09:14:46 +0530

    called food_list and food_category and made them globally available

--- db.js
+++ db.js
@@ -1,17 +1,40 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
 
-
 const mongoURI = process.env.mongoURI;
 
-const mongoConnection = async () =>{
-    try{
+const mongoConnection = async () => {
+    try {
         await mongoose.connect(mongoURI);
-        console.log("Database has been successfully");
-    } catch (err) {
-        console.log(err.message);
-    }
-}
+        console.log("Database connected successfully");
 
+        try {
+            const fetched_data = mongoose.connection.db.collection("food_lists");
+
+            try {
+                const data = await fetched_data.find({}).toArray();
+
+                const food_category = mongoose.connection.db.collection("food_category");
+                const catData = await food_category.find({}).toArray();
+
+                global.food_lists = data;
+                global.food_category = catData;
+
+                // console.log("Global food lists:", global.food_lists);
+                // console.log("Global food categories:", global.food_categories);
+
+            } catch (err) {
+                console.error("Error fetching data:", err.message);
+            }
+
+
+
+        } catch (err) {
+            console.error("Error fetching data:", err.message);
+        }
+    } catch (err) {
+        console.log("Database connection error:", err.message);
+    }
+};
 
 module.exports = mongoConnection;
