12e21fa7fabf99dcb4e3cf14b207e2d4ade2c1f2
tewarishash12 <tewarishashwat2002@gmail.com>
2024-09-29 12:21:28 +0530

    created a controller function for calling fooditems data from the database

--- routes/contentRoutes.js
+++ routes/contentRoutes.js
@@ -1,12 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const contentController = require("../controllers/contentController");
 
-router.post("/food", async(req,res) =>{
-    try{
-        res.send([global.food_lists, global.food_category])
-    } catch(err){
-        console.log(err.message);
-    }
-})
+router.post("/food", contentController.orderList)
 
 module.exports = router;
